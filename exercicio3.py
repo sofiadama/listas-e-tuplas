e36d8223b5b8edb2a52d9dffed9a23451272e549
SOFIA <sofiadamasin@gmail.com>
2024-10-02 19:56:13 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,10 +1,4 @@
 numeros = (17, 28, 94, 57, 13, 16, 68, 2, 37, 49)
 
-maior_numero = max(numeros)
-menor_numero = min(numeros)
-
-posiçao_maior = numeros.index(maior_numero)
-posiçao_menor = numeros.index(menor_numero)
-
-print(f'O maior elemento é {maior_numero} e ocupa a {posiçao_maior + 1}º posição.')
-print(f'O menor elemento é {menor_numero} e ocupa a {posiçao_menor + 1}º posição.')
+print(f'O maior elemento é {max(numeros)} e ocupa a {numeros.index(max(numeros)) + 1}º posição.')
+print(f'O menor elemento é {min(numeros)} e ocupa a {numeros.index(min(numeros)) + 1}º posição.')
